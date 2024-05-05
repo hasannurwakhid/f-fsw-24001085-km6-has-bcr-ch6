ae6e79cf0fed4d34f393cb8e1e35e895234bbb28
Hasan Nur Wakhid <hasannurwakhid@gmail.com>
2024-05-05 23:52:19 +0700

    first commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,6 +3,6 @@ import reducers from "./reducers";
 
 // this will make a global state/temporary database in frontend
 export default configureStore({
-  devTools: true, // to enable/disable devtools in chrome/firefox/etc
+  devTools: false, // to enable/disable devtools in chrome/firefox/etc
   reducer: reducers, // state database
 });
